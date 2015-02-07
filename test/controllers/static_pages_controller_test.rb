0f0417da47c88d8b37f4fa77867b74bc062d655a
pooja gangwani <gangwani.pooja@yahoo.com>
2015-02-07 18:24:08 +0530

    database connectivity done

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "Home | GitLancer"
+  end
+
+  test "should get help" do
+    get :help
+    assert_response :success
+    assert_select "title", "Home | GitLancer"
+  end
+
+
+end
